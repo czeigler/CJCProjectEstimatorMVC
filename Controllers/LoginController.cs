58d1942a5ead87e225c32381d513ad7f09940545
Chris Zeigler <czeigler@gmail.com>
2013-04-09 21:48:26 -0600

    added new files

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CJCProjectEstimatorMVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        //
+        // GET: /Login/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login()
+        {
+            return RedirectToAction("Index", "ProjectHome");
+        }
+
+        [HttpPost]
+        public ActionResult Logout()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+    }
+}
---
+++ Controllers/ProjectHomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CJCProjectEstimatorMVC.Controllers
+{
+    public class ProjectHomeController : Controller
+    {
+        //
+        // GET: /ProjectHome/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
