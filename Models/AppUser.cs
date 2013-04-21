f68cbbe9312b6bbc13b4d7f209716fcb73aae625
Chris Zeigler <czeigler@gmail.com>
2013-04-21 16:25:33 -0600

    changes

--- Models/AppUser.cs
+++ Models/AppUser.cs
@@ -14,17 +14,28 @@ namespace CJCProjectEstimatorMVC.Models
         [Key]
         [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-        
+
+        [Required(ErrorMessage = "Please enter a User Name")]
+        [StringLength(40, ErrorMessage = "User Name is too long")]
         public String UserName { get; set; }
 
+        [Required(ErrorMessage = "Please enter a First Name")]
+        [StringLength(40, ErrorMessage = "First Name is too long")]
         public String FirstName { get; set; }
 
+        [Required(ErrorMessage = "Please enter a Last Name")]
+        [StringLength(40, ErrorMessage = "Last Name is too long")]
         public String LastName { get; set; }
 
+        [Required(ErrorMessage = "Please enter a Password")]
+        [StringLength(256, ErrorMessage = "Password is too long")]
         public String PasswordHash { get; set; }
 
+        [Required]
+        [StringLength(256)]
         public String PasswordSalt { get; set; }
 
+        [StringLength(40, ErrorMessage = "Company Name is too long")]
         public String CompanyName { get; set; }
 
     }
