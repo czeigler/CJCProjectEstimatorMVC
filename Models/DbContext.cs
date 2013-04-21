21fab12b2c5dac8aa79b86a4fdb5d6cbee809d61
Chris Zeigler <czeigler@gmail.com>
2013-04-21 16:02:22 -0600

    added some files create working in FireFox at least

---
+++ Models/DbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace CJCProjectEstimatorMVC.Models
+{
+    public class DBContext : DbContext
+    {
+        public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<Material> Materials { get; set; }
+        public DbSet<MaterialUnit> MaterialsUnits { get; set; }
+        public DbSet<Project> Projects { get; set; }
+
+    }
+
+
+}
