ac6cedab8f4c24763875e3d4fab3d1e288252854
Chris Zeigler <czeigler@gmail.com>
2013-04-22 00:07:20 -0600

    created some new stuff

---
+++ Controllers/ProjectListController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CJCProjectEstimatorMVC.Models;
+using System.Data.Objects;
+
+namespace CJCProjectEstimatorMVC.Controllers
+{
+    public class ProjectListController : BaseController
+    {
+        //
+        // GET: /ProjectList/
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            DBContext db = new DBContext();
+
+            ProjectListViewModel projectListViewModel = new ProjectListViewModel();
+
+            projectListViewModel.Projects = db.Projects.ToList();
+
+            return View(projectListViewModel);
+        }
+
+    }
+}
