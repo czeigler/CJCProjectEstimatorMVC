4c7be039c89290be8745b70f0f0f3aba51f5bdbc
Chris Zeigler <czeigler@gmail.com>
2013-05-04 19:35:16 -0600

    restoring some files

--- Controllers/ProjectListController.cs
+++ Controllers/ProjectListController.cs
@@ -15,13 +15,20 @@ namespace CJCProjectEstimatorMVC.Controllers
         // GET: /ProjectList/
 
         [HttpGet]
-        public ActionResult Index()
+        public ActionResult Index(String projectName)
         {
             DBContext db = new DBContext();
 
             ProjectListViewModel projectListViewModel = new ProjectListViewModel();
             Int32 currentUserId = (Int32)getCurrentUserId() + 0;
-            projectListViewModel.Projects = db.Projects.Where(p => p.AppUserId == currentUserId).ToList<Project>();
+            if (projectName != null && projectName.Length > 0)
+            {
+                projectListViewModel.Projects = db.Projects.Where(p => p.AppUserId == currentUserId).Where(p => p.Name.Contains(projectName)).ToList<Project>();
+            }
+            else
+            {
+                projectListViewModel.Projects = db.Projects.Where(p => p.AppUserId == currentUserId).ToList<Project>();
+            }
 
             return View(projectListViewModel);
         }
